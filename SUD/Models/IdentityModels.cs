451c4d605028fe82cce197ea9a9a8eb6f63b74f7
Roberto Mansilla <r.mansilla05@gmail.com>
2018-04-27 21:16:18 -0600

    Se crearon los modelos User, Rol, RolPermission, los controladores web y api del area Gestion de Usuario, por el momento solo tienen la funcionalidad con la que fueron generados y las vistas de los controladores web

--- SUD/Models/IdentityModels.cs
+++ SUD/Models/IdentityModels.cs
@@ -29,5 +29,11 @@ namespace SUD.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<SUD.Models.Rol> Rols { get; set; }
+        //No se generaba al crear el controlador
+        public System.Data.Entity.DbSet<SUD.Models.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<SUD.Models.RolPermission> RolPermissions { get; set; }
     }
 }
