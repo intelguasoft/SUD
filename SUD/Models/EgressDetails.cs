da7b2977a22748bd09c17b62f9d3301440efb1ee
@intelguasoft <hnrdiaz@gmail.com>
2018-05-02 23:33:05 -0600

    Se agregaron los modelos del area de Compras

---
+++ SUD/Models/EgressDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUD.Models
+{
+    public class EgressDetails
+    {
+
+        public int EgressDetailsId { get; set; }
+
+        public int EgressId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string Description { get; set; }
+
+        public float Quantity { get; set; }
+
+        public int KardexId { get; set; }
+
+        public virtual Egress Egress { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual Kardex Kardex { get; set; }
+
+
+    }
+}
---
+++ SUD/Models/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUD.Models
+{
+    public class Purchase
+    {
+
+        public int PurchaseId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public int CellarId { get; set; }
+
+        public virtual Supplier Supplier { get; set; }
+
+        public virtual Cellar Cellar { get; set; }
+    }
+}
